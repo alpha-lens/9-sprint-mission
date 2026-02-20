bed140683e366eefcefd7bcb4974873cb2141398
Alpha <mathematic026@gmail.com>
2026-02-20 16:44:49 +0900

    Refact: BinaryContent API

--- discordit/src/main/java/com/sprint/mission/discordit/controller/BinaryContentController.java
+++ discordit/src/main/java/com/sprint/mission/discordit/controller/BinaryContentController.java
@@ -2,34 +2,35 @@ package com.sprint.mission.discordit.controller;
 
 import com.sprint.mission.discordit.entity.BinaryContent;
 import com.sprint.mission.discordit.service.BinaryContentService;
+import java.util.List;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Controller
 @ResponseBody
-@RequestMapping("/api/binaryContent")
+@RequestMapping("/api/binaryContents")
 public class BinaryContentController {
 
   private final BinaryContentService binaryContentService;
 
-  @RequestMapping(path = "find")
-  public ResponseEntity<BinaryContent> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+  @RequestMapping(path = "{binaryContentId}", method = RequestMethod.GET)
+  public ResponseEntity<BinaryContent> find(@PathVariable UUID binaryContentId) {
     BinaryContent binaryContent = binaryContentService.find(binaryContentId);
     return ResponseEntity
         .status(HttpStatus.OK)
         .body(binaryContent);
   }
 
-  @RequestMapping(path = "findAllByIdIn")
+  @RequestMapping(method = RequestMethod.GET)
   public ResponseEntity<List<BinaryContent>> findAllByIdIn(
       @RequestParam("binaryContentIds") List<UUID> binaryContentIds) {
     List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
