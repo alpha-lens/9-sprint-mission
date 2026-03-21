553d8e3578b32c957f28bbc5c8615925cb062a31
Alpha <mathematic026@gmail.com>
2026-03-21 22:35:05 +0900

    Fix: schema.sql binary_contents에서 bytes가 사용되고 있는 것을 삭제함.
    ref: https://github.com/codeit-bootcamp-spring/9-sprint-mission/pull/119#discussion_r2952660128

--- discodeit/src/main/resources/schema.sql
+++ discodeit/src/main/resources/schema.sql
@@ -11,8 +11,7 @@ CREATE TABLE binary_contents (
      created_at TIMESTAMPTZ NOT NULL,
      file_name VARCHAR(255) NOT NULL,
      size BIGINT NOT NULL,
-     content_type VARCHAR(100) NOT NULL,
-     bytes BYTEA NOT NULL
+     content_type VARCHAR(100) NOT NULL
 );
 
 -- 4. users 테이블
