540f9e019d15c438c4955b9878f4c5b9bf516248
Alpha <mathematic026@gmail.com>
2026-03-21 22:36:34 +0900

    Refactor: DiscodeitApplication 불필요한 의존성 삭제

--- discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,9 +2,7 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
 public class DiscodeitApplication {
 
