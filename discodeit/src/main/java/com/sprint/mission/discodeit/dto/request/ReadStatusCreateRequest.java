5cf9e4ca4492a93884fc5762a417a1ce62cd0f85
Alpha <mathematic026@gmail.com>
2026-03-26 17:38:06 +0900

    Feat: ReadStatusController valid 추가

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
@@ -1,14 +1,19 @@
 package com.sprint.mission.discodeit.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.UUID;
 
 public record ReadStatusCreateRequest(
+    @NotNull @NotBlank
     @Schema(description = "사용자 ID")
     UUID userId,
+    @NotNull @NotBlank
     @Schema(description = "채널 ID")
     UUID channelId,
+    @NotNull @NotBlank
     @Schema(description = "읽음 시간")
     Instant lastReadAt
 ) {
