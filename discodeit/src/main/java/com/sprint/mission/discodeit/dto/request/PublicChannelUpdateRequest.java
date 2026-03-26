3353f0db20e92f530c3eb3e651e21e177d215e35
Alpha <mathematic026@gmail.com>
2026-03-26 16:43:16 +0900

    Feat: DTO에 valid 기능을 추가

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
@@ -1,6 +1,10 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record PublicChannelUpdateRequest(
+    @NotNull @NotBlank
     String newName,
     String newDescription
 ) {
