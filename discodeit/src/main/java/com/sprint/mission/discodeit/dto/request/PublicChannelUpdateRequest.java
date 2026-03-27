3674ebd004b24e5dfdb15e4c8954c546dc8565dc
Alpha <mathematic026@gmail.com>
2026-03-27 10:24:25 +0900

    Refactor: newName valid 옵션 수정

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelUpdateRequest.java
@@ -1,10 +1,9 @@
 package com.sprint.mission.discodeit.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record PublicChannelUpdateRequest(
-    @NotNull @NotBlank
+    @NotBlank
     String newName,
     String newDescription
 ) {
