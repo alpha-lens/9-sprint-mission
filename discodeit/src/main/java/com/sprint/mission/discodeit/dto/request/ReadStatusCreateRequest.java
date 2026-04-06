e361e2ac2b05a9661da242b3cb2aa8e6e78f4ea2
Alpha <mathematic026@gmail.com>
2026-04-06 14:15:17 +0900

    Fix: UUID에 NotBlank가 들어간 부분 수정 https://github.com/codeit-bootcamp-spring/9-sprint-mission/pull/145#discussion_r3037025255

--- discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
+++ discodeit/src/main/java/com/sprint/mission/discodeit/dto/request/ReadStatusCreateRequest.java
@@ -1,19 +1,18 @@
 package com.sprint.mission.discodeit.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.Instant;
 import java.util.UUID;
 
 public record ReadStatusCreateRequest(
-    @NotNull @NotBlank
+    @NotNull
     @Schema(description = "사용자 ID")
     UUID userId,
-    @NotNull @NotBlank
+    @NotNull
     @Schema(description = "채널 ID")
     UUID channelId,
-    @NotNull @NotBlank
+    @NotNull
     @Schema(description = "읽음 시간")
     Instant lastReadAt
 ) {
