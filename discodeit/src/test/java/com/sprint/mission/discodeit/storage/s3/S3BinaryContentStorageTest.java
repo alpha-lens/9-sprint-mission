c8741fe2f4f8297f9dd20f1b91be4fea962fde84
Alpha <mathematic026@gmail.com>
2026-04-09 11:36:05 +0900

    Test: TestProfile을 DiscodeitApplicationTests로 옮기기 위해 제거

--- discodeit/src/test/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorageTest.java
+++ discodeit/src/test/java/com/sprint/mission/discodeit/storage/s3/S3BinaryContentStorageTest.java
@@ -14,10 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = "discodeit.storage.type=s3")
-@ActiveProfiles("test")
 class S3BinaryContentStorageTest {
 
   @BeforeAll
